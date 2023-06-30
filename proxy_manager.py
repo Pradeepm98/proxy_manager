189dd060e4bc3e987ab48b5c175ac30314c8ccdf
Pradeepm98 <132885368+Pradeepm98@users.noreply.github.com>
2023-06-30 10:54:02 +0530

    Update proxy_manager.py

--- proxy_manager.py
+++ proxy_manager.py
@@ -1,59 +1,44 @@
-# %%
 import socket
 
-# Create a socket object
 dynamic = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dynamic.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Get the local machine name and define a port for communication
-host = "0.0.0.0"#socket.gethostname()
+host = "0.0.0.0"
 port = 30000
 
-# Bind the socket to a specific address and port
 dynamic.bind((host, port))
 print("Server is listening on {}:{}".format(host, port))
 
-# Listen for incoming connections
 dynamic.listen(5)
-
 print("Server is listening on {}:{}".format(host, port))
 
-# %%
-import socket
-
-# Create a socket object
 proxy_manager = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+proxy_manager.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Get the local machine name and define a port for communication
-host = "0.0.0.0"#socket.gethostname()
+host = "0.0.0.0"
 port = 30001
 
-# Bind the socket to a specific address and port
 proxy_manager.bind((host, port))
 print("Server is listening on {}:{}".format(host, port))
 
-# Listen for incoming connections
 proxy_manager.listen(5)
-
 print("Server is listening on {}:{}".format(host, port))
 
-# %%
-dynamic_server, addr = dynamic.accept() 
-print("Received connection from: {}".format(addr))
+try:
+    dynamic_server, addr = dynamic.accept()
+    print("Received connection from: {}".format(addr))
 
-# %%
-proxy_client, addr = proxy_manager.accept() 
-print("Received connection from: {}".format(addr))
+    proxy_client, addr = proxy_manager.accept()
+    print("Received connection from: {}".format(addr))
 
-# %%
+    data = proxy_client.recv(10240)
+    dynamic_server.send(data)
 
-data = proxy_client.recv(10240)
+    finaldata = dynamic_server.recv(10240)
+    proxy_client.send(finaldata)
 
-dynamic_server.send(data)
-
-finaldata=dynamic_server.recv(10240)
-
-proxy_client.send(finaldata)
-
-
-
-
+finally:
+    dynamic_server.close()
+    proxy_client.close()
+    dynamic.close()
+    proxy_manager.close()
