68fe943382b438bfc153d90f14a710f8bde4de69
Pradeepm98 <132885368+Pradeepm98@users.noreply.github.com>
2023-06-30 15:45:53 +0530

    Update proxy_manager.py

--- proxy_manager.py
+++ proxy_manager.py
@@ -4,41 +4,46 @@ dynamic = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dynamic.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 host = "0.0.0.0"
-port = 30000
+port = 3000
 
 dynamic.bind((host, port))
 print("Server is listening on {}:{}".format(host, port))
 
-dynamic.listen(5)
+dynamic.listen(1)
 print("Server is listening on {}:{}".format(host, port))
 
 proxy_manager = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 proxy_manager.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 host = "0.0.0.0"
-port = 30001
+port = 3001
 
 proxy_manager.bind((host, port))
 print("Server is listening on {}:{}".format(host, port))
 
-proxy_manager.listen(5)
+proxy_manager.listen(1)
 print("Server is listening on {}:{}".format(host, port))
 
-try:
-    dynamic_server, addr = dynamic.accept()
-    print("Received connection from: {}".format(addr))
+dynamic_server, addr = dynamic.accept()
+print("Received connection from: {}".format(addr))
+
+def proxy_manage():
+
+
 
     proxy_client, addr = proxy_manager.accept()
     print("Received connection from: {}".format(addr))
 
-    data = proxy_client.recv(10240)
+
+    data = proxy_client.recv(1024)
     dynamic_server.send(data)
 
-    finaldata = dynamic_server.recv(10240)
+    finaldata = dynamic_server.recv(1024)
     proxy_client.send(finaldata)
 
-finally:
-    dynamic_server.close()
+    #dynamic_server.close()
     proxy_client.close()
-    dynamic.close()
-    proxy_manager.close()
+
+
+while True: 
+   proxy_manage()
