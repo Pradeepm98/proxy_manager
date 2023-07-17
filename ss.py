09cab769ec80afcf0aace5b07d341a7406d940a6
Pradeepm98 <132885368+Pradeepm98@users.noreply.github.com>
2023-07-17 13:46:48 +0530

    Update ss.py

--- ss.py
+++ ss.py
@@ -21,6 +21,7 @@ def start_server():
     port = 30000
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Reuse address
     server_socket.bind((host, port))
     server_socket.listen(1)
     print("Server listening on {}:{}".format(host, port))
