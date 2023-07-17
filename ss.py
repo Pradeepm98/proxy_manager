03b83c7f68c6a37b511f1f1f2ed3bed3121251ad
Pradeepm98 <132885368+Pradeepm98@users.noreply.github.com>
2023-07-17 13:44:56 +0530

    Update ss.py

--- ss.py
+++ ss.py
@@ -17,8 +17,8 @@ def handle_client(client_socket):
     client_socket.close()
 
 def start_server():
-    host = '127.0.0.1'  # localhost
-    port = 8000
+    host = '0.0.0.0'  # localhost
+    port = 30000
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
