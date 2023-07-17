b8ebe1d1b2fa79415b5454d6bfb511b61e6352fc
Pradeepm98 <132885368+Pradeepm98@users.noreply.github.com>
2023-07-17 13:44:08 +0530

    Create ss.py

---
+++ ss.py
@@ -0,0 +1,32 @@
+import socket
+
+def handle_client(client_socket):
+    print("New client connected")
+    client_socket.send(b"Hi")
+    
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                print("Client disconnected")
+                break
+        except ConnectionResetError:
+            print("Client forcibly closed the connection")
+            break
+
+    client_socket.close()
+
+def start_server():
+    host = '127.0.0.1'  # localhost
+    port = 8000
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    print("Server listening on {}:{}".format(host, port))
+
+    while True:
+        client_socket, address = server_socket.accept()
+        handle_client(client_socket)
+
+start_server()
